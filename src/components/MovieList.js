5a93b87446ff9139476e843000645060f1a87e5b
Jayson Byrd <jayson_byrd_95@outlook.com>
2021-12-07 17:32:16 -0500

    modified:   README.md 	modified:   src/components/Movie.js 	modified:   src/components/MovieList.js

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -4,7 +4,7 @@ import MovieListItem from './MovieListItem';
 import MovieFooter from './MovieFooter';
 
 const MovieList = (props)=> {
-    const movies = [];
+    //const movies = [];
 
     return (
         <div className="col">
@@ -21,12 +21,12 @@ const MovieList = (props)=> {
 
                 <tbody>
                     {
-                        movies.map(movie=><MovieListItem key={movie.id} movie={movie}/>)
+                        props.movies.map(movie=><MovieListItem key={movie.id} movie={movie}/>)
                     }
                 </tbody>
             </table>
             
-            <MovieFooter totalMovies={movies.length}/>
+            <MovieFooter totalMovies={props.movies.length}/>
         </div>
     );
 }
